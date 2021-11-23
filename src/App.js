0bc40e9dcad1f01b7595a19f9d76bb80ab16bd20
Josh Smith <josh@jshsmth.com>
2021-11-23 21:26:06 +1100

    added footer to finder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import SearchForm from "./components/SearchForm";
 
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BumbagProvider>
       <Navbar />
       <SearchForm />
+      <Footer />
     </BumbagProvider>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import { Box } from 'bumbag';
+import { Image } from 'bumbag';
+import logo from './logo-light.png'
+
+function Footer() {
+    return (
+        <div>
+            <Box alignX='center'>
+            <p>Created with React UI Libary</p>
+            <Image
+  fit="contain"
+  height="100px"
+  width="100px"
+  src={logo}
+  alt="Bumgbag Ui Logo"
+  backgroundColor="white"
+/>
+            </Box>
+        </div>
+    )
+}
+
+
+export default Footer
